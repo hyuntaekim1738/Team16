14192f9ac08d8c058c7b99b06c7a80a739d316ea
Hyun Tae Kim <108547741+hyuntaekim1738@users.noreply.github.com>
2026-03-24 13:52:16 -0500

    time travel logic added

---
+++ Assets/Scripts/TimeTravelConfirmationMessage.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class TimeTravelConfirmationMessage : MonoBehaviour
+{
+    private string targetScene;
+    private int selectedIndex = 0;
+
+    public TextMeshProUGUI eraText;
+    public CharacterMovement characterScript;
+    public Button confirmButton;
+    public Button cancelButton;
+    public bool joyStickMode;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (joyStickMode)
+        {
+            horizontal = Input.GetAxis("Vertical");
+        }
+
+        if (horizontal < -0.5f)
+        {
+            selectedIndex = 0;
+            UpdateButtonHighlight();
+        }
+        else if (horizontal > 0.5f)
+        {
+            selectedIndex = 1;
+            UpdateButtonHighlight();
+        }
+
+        if ( Input.GetKeyDown(KeyCode.B)) //Input.GetButtonDown("js0") ||
+        {
+            if (selectedIndex == 0)
+            {
+                Confirm();
+            }    
+            else
+            {
+                Cancel();
+            }
+                
+        }
+    }
+
+    void OnEnable()
+    {
+        characterScript.enabled = false;
+    }
+
+    void OnDisable()
+    {
+        characterScript.enabled = true;
+    }
+
+    public void Setup(string eraName, string scene)
+    {
+        targetScene = scene;
+        eraText.text = "Would you like to travel to era: " + eraName + "?";
+        selectedIndex = 0;
+        UpdateButtonHighlight();
+    }
+
+    public void Confirm()
+    {
+        Debug.Log("CONFIRMING, targetScene = '" + targetScene + "'");
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void Cancel()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void UpdateButtonHighlight()
+    {
+        ColorBlock selected = ColorBlock.defaultColorBlock;
+        selected.normalColor = Color.yellow;
+
+        ColorBlock normal = ColorBlock.defaultColorBlock;
+        normal.normalColor = Color.white;
+
+        confirmButton.colors = (selectedIndex == 0) ? selected : normal;
+        cancelButton.colors = (selectedIndex == 1) ? selected : normal;
+        Debug.Log(selectedIndex);
+    }
+
+}
