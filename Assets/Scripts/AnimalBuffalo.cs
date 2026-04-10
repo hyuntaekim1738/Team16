39dffb5afc9fe6ae59e052ee006520bd39d741b3
uma-i <umainam6@gmail.com>
2026-04-09 21:03:20 -0500

    fixed bugs

--- Assets/Scripts/AnimalBuffalo.cs
+++ Assets/Scripts/AnimalBuffalo.cs
@@ -3,12 +3,8 @@ using static ExitMenuButton;
 using static FeedFood;
 using static Food;
 using static FeedFoodLeaf;
-
-
-public class Animal : MonoBehaviour
+public class AnimalBuffalo : MonoBehaviour
 {
-    RaycastHit hit;
-    Ray ray;
     public GameObject menu;
     public GameObject character;
     public GameObject camera;
@@ -16,7 +12,6 @@ public class Animal : MonoBehaviour
     bool isMenuOpen = false;
     bool feeding = false;
     bool feedingDone, feedingDoneL = false;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,24 +24,21 @@ public class Animal : MonoBehaviour
         feeding = foodPickUp();
         feedingDone = getFeedFood();
         feedingDoneL = getFeedFoodL();
-        // ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-        // if(Physics.Raycast(ray, out hit))
-        // {
-            if (isPointerEnter)
+        if (isPointerEnter)
             {
                 gameObject.GetComponent<Outline>().enabled = true;
                 if (Input.GetButton("js1"))
                 {   
                     if(!feeding){
-                        // Debug.Log("hi");
+                        // Debug.Log("menu open");
                         menu.SetActive(true);
                         isMenuOpen = true;
                         character.GetComponent<CharacterMovement>().enabled = false;
                         camera.GetComponent<CameraOperations>().enabled = false;
                     }
-                    else if (feedingDone)
+                    else if (feedingDoneL)
                     {
-                        // Debug.Log("hi done");
+                        // Debug.Log("leaf done");
                         feeding = foodPickUp();
                             if (Input.GetButtonDown("js1"))
                             {
@@ -55,34 +47,22 @@ public class Animal : MonoBehaviour
                                 character.GetComponent<CharacterMovement>().enabled = false;
                                 camera.GetComponent<CameraOperations>().enabled = false;
                             }
-                            
                     }
                 }
             }
-            else
-            {
-                gameObject.GetComponent<Outline>().enabled = false;
-            }
-        
+        else
+        {
+            gameObject.GetComponent<Outline>().enabled = false;
+        }
         if (animalMenuClosed())
         {
             isMenuOpen = false;
         }
-        // feeding = getFeedFood();
-        // }
-        // else
-        // {
-        //     gameObject.GetComponent<Outline>().enabled = false;
-        // }
-        // }
     }
-
     public void OnPointerEnter()
     {
         if(!isMenuOpen)
             isPointerEnter = true;
-        // if (Input.GetButton("js5"))
-        //     menu.SetActive(true);
     }
 
     public void OnPointerExit()
