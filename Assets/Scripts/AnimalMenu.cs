5d91a1c70ae3cd33afe79f337db05a113d44db66
uma-i <umainam6@gmail.com>
2026-05-01 20:32:28 -0500

    animal menu improved, edited readme

--- Assets/Scripts/AnimalMenu.cs
+++ Assets/Scripts/AnimalMenu.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 public class AnimalMenu : MonoBehaviour
 {
+    Transform camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        camera = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        // transform.position = camera.position + camera.forward * 2f + camera.up * 0.45f;
+        transform.position = camera.position + new Vector3(camera.forward.x, 0, camera.forward.z).normalized * 2f;
+        // transform.rotation = Camera.main.transform.rotation;
+        transform.LookAt(transform.position + camera.forward, camera.up);
+        // transform.LookAt(new Vector3(camera.position.x, transform.position.y, camera.position.z));
     }
 }
